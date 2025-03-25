f76a922f4e5fdb279d0f2d78088704ed37948439
Andrew Kim <Andkim123@gmail.com>
2025-03-25 11:06:07 -0700

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
-      <h1 className="text-4xl font-bold underline">Tailwind is working 🎉</h1>
+    <div className="min-h-screen bg-white text-black">
+      <h1 className="text-2xl font-bold text-center pt-10">Hello World</h1>
     </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        Testing 1 2 3
-        Last CICD test
-      </p>
-    </>
   )
 }
 
